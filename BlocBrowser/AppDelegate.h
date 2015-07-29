665238b4d77feda55b37a40d67fc531eb1213f2d
Jeffrey Isaray <ji8521@gmail.com>
2015-07-28 23:28:19 -0400

    Assignment 23 completed

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Jeffrey Isaray on 7/27/15.
+//  Copyright (c) 2015 Jeffrey Isaray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Jeffrey Isaray on 7/27/15.
+//  Copyright (c) 2015 Jeffrey Isaray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
